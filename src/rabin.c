b56a66b5cc6d1e0b872843befe5043461f1b3bdc
Christian Schwede <info@cschwede.de>
2013-01-28 19:29:57 +0100

    Using linked list for cyclic window, improved code

--- src/rabin.c
+++ src/rabin.c
@@ -19,6 +19,7 @@ limitations under the License. */
 /*  Simple utility to calculate variable sized blocks of an input file.
     Uses Rabin fingerprint to determine block boundaries.  */
 
+
 const int PRIME = 3;
 const int WINDOWSIZE = 48;
 const unsigned int AVGSIZE = 64*1024-1;
@@ -26,9 +27,9 @@ const int MAXSIZE = 256*1024;
 const int MINSIZE = 8*1024;
 
 
-struct listelement {
-    unsigned long long size;
-    struct listelement *next;
+struct node {
+    unsigned long long value;
+    struct node *next;
 };
 
 
@@ -44,114 +45,92 @@ unsigned long long *char_nth_prime(int windowsize, int prime) {
 }
 
 
-struct listelement* rabin(char *filename) {
-    unsigned long long *map = char_nth_prime(WINDOWSIZE, PRIME);
-
-    struct listelement *head = NULL;
-    struct listelement *curr = NULL; 
-
-    head = malloc(sizeof(struct listelement));  
-    curr = head;
-
-    curr->size = 0;
-
+struct node* rabin(char *filename) {
     FILE *fp;
     fp = fopen(filename, "rb");
     if (!fp) { PyErr_SetFromErrno(PyExc_IOError); return NULL; }
 
-    //inizalize ring
-    int i=0;
-    int window[WINDOWSIZE];
-    for (i = 0; i <= WINDOWSIZE; i++) { window[i] = 0; }
+    unsigned long long *map = char_nth_prime(WINDOWSIZE, PRIME);
+
+    // initialize linked list for returned blocksizes
+    struct node *head = NULL;
+    struct node *curr = NULL; 
+    head = malloc(sizeof(struct node));  
+    curr = head;
+    curr->value = 0;
     
-    unsigned long long current_position=0;
-    unsigned long long last_position=0;
+    // inizalize cyclic ring
+    struct node *cycle_head = NULL;
+    struct node *cycle_curr = NULL; 
+    cycle_head = malloc(sizeof(struct node));  
+    cycle_curr = cycle_head;
+    cycle_curr->value = 0;
+    int i = 0;
+    for (i = 0; i < (WINDOWSIZE-1); i++) {
+        cycle_curr->next = malloc(sizeof(struct node)); 
+        cycle_curr = cycle_curr->next;
+        cycle_curr->value = 0;
+    }
+    cycle_curr->next = cycle_head; //close the ring
 
     int ch=0;
-    unsigned long long fingerprint=0;
-    unsigned long long oldest_byte = 0;
-    int newest_byte_position=0;
-	int oldest_byte_position=1;
+    unsigned long long fingerprint = 0;
     unsigned long long blocksize = 0;
     while (ch != EOF) {
     	ch=getc(fp);
-        
-        if (newest_byte_position > WINDOWSIZE) { newest_byte_position=0; }
-        if (oldest_byte_position > WINDOWSIZE) { oldest_byte_position=0; }
-        
-        window[newest_byte_position]=ch;
-        oldest_byte = window[oldest_byte_position];
-        
+
         fingerprint *= PRIME;
         fingerprint += ch;
-        fingerprint -= map[oldest_byte];
+        fingerprint -= map[cycle_curr->value];
         
-        blocksize = current_position - last_position;
+        cycle_curr->value = ch;
+        cycle_curr = cycle_curr->next;
+
         if (blocksize > MINSIZE) {
-            if (((fingerprint & AVGSIZE) == (AVGSIZE-1)) || 
-                (blocksize > MAXSIZE)) {
-                    if (curr->size!=0) { //only 0 if first element in list
-                        curr->next = malloc(sizeof(struct listelement)); 
-                        curr = curr->next;
-                        curr->next = NULL;
-                    }
-                    curr->size = blocksize;
-                    last_position = current_position;
+            if (((fingerprint & AVGSIZE) == 1) || (blocksize > MAXSIZE)) {
+                curr->value = blocksize;
+                curr->next = malloc(sizeof(struct node)); 
+                curr = curr->next;
+                curr->next = NULL;
+                blocksize=0;
             }
         }
-        current_position++;
-        newest_byte_position++; 
-        oldest_byte_position++;
+        blocksize++;
     }
     fclose(fp);
-
     return head;
-}
-
-
-void print_rabin(char *filename) {
-    struct listelement* head = rabin(filename);
-    struct listelement *curr = head;
-    while (curr != NULL) {
-        printf("%llu\n", curr->size);
-        curr = curr->next;
-    }
-}
-
-
-int main(int argc, char *argv[]){
-    if (argc != 2) {
-        printf("Missing filename!\n");
-        return 1;
-    }
-    print_rabin(argv[1]);
-    return 0;
 }
 
 
 static PyObject * pyrabin(PyObject *self, PyObject *args) {
     const char *filename;
     if (!PyArg_ParseTuple(args, "s", &filename)) { return NULL; }
-    struct listelement* head = rabin(filename);
-    
-    if (head == NULL) { return NULL; } //returns IOError
-    
-    struct listelement *curr = head;
-    PyObject *list, *pylistelement;
+
+    PyObject *list;
+    PyObject *pynode = NULL;
     if (!(list=PyList_New(0))) return NULL;
-    while (curr != NULL) {
-        pylistelement=Py_BuildValue("l", curr->size);
-        PyList_Append(list, pylistelement);
+    
+    struct node* curr = rabin(filename);
+
+    if (curr == NULL) { return NULL; } //returns IOError
+    while (curr->next != NULL) {
+        pynode=Py_BuildValue("l", curr->value);
+        PyList_Append(list, pynode);
         curr = curr->next;
     }
-    Py_DECREF(pylistelement);
+    Py_DECREF(pynode);
     return list;
 }
 
+
 static PyMethodDef IndexerMethods[] = {
-    {"rabin",  pyrabin, METH_VARARGS, "Rabin fingerprinting a file"},
+    {"rabin", 
+    pyrabin, 
+    METH_VARARGS, 
+    "Rabin fingerprinting a file"},
     {NULL, NULL, 0, NULL}     
 };
+
 
 PyMODINIT_FUNC initrabin(void) {
         (void) Py_InitModule("rabin", IndexerMethods);
