ab63395c1d8c9ec6bce13811965c8335463b0b78
Christian Schwede <info@cschwede.de>
2014-04-15 20:31:44 +0200

    Fix compile error on OS X 10.9

--- setup.py
+++ setup.py
@@ -1,3 +1,5 @@
 from distutils.core import setup, Extension
+import os
 
+os.environ['CFLAGS'] = "-Qunused-arguments"
 setup(name = "Indexer", version = "0.1", ext_modules = [Extension("rabin", ["src/rabin.c", ])])
