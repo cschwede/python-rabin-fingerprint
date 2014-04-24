10b06682c31e0bfa61611732b7e25ffa8d964b92
Christian Schwede <info@cschwede.de>
2014-04-24 20:25:04 +0200

    Add travis-ci.org test, change compiler options
    Changed compiler options:
    - use C99 mode
    - use gcc option "-Qunused-arguments" only on OS X

--- setup.py
+++ setup.py
@@ -1,5 +1,9 @@
 from distutils.core import setup, Extension
 import os
+from sys import platform 
 
-os.environ['CFLAGS'] = "-Qunused-arguments"
+os.environ['CFLAGS'] = "-std=c99"
+if platform == "darwin":
+    os.environ['CFLAGS'] += " -Qunused-arguments"
+
 setup(name = "Indexer", version = "0.1", ext_modules = [Extension("rabin", ["src/rabin.c", ])])
